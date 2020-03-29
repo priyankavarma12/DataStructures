b2edd59224be5fa28fda766a96e840d0536cee9e
priyankavarma12 <55535848+priyankavarma12@users.noreply.github.com>
2020-03-29 19:18:36 +0530

    Added merge two binary trees
    Added merge two binary trees

--- Trees/BinaryTreeUtil.java
+++ Trees/BinaryTreeUtil.java
@@ -99,4 +99,15 @@ public class BinaryTreeUtil {
         }
         return hasPathSum(root.left, sum - root.data) || hasPathSum(root.right, sum - root.data);
     }
+     
+     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null)
+            return t2;
+        if (t2 == null)
+            return t1;
+        t1.data += t2.data;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
 }
